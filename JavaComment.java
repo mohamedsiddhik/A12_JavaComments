354fc7480ad8086d30057423326ea5cc7b5410c5
mohamedsiddhik <117736610+mohamedsiddhik@users.noreply.github.com>
2022-11-30 15:58:53 +0530

    Add files via upload

---
+++ JavaComment.java
@@ -0,0 +1,10 @@
+
+
+//Single- line Comment
+
+public class JavaComment {
+    public static void main(String[] args) {
+        int i=10; // i is a variable with value 10
+        System.out.println(i);  //printing the variable i
+    }
+}
---
+++ JavaComment2.java
@@ -0,0 +1,15 @@
+
+
+//Multi-line Comment
+
+public class JavaComment2 {
+    public static void main(String[] args) {
+/* Let's declare and
+ print variable in java. */
+        int i=10;
+        System.out.println(i);
+/* float j = 5.9;
+    float k = 4.4;
+    System.out.println( j + k ); */
+    }
+}
